6a9fa3914a6a2e119f620ec58e95c82eab85b407
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-11-15 11:08:03 +0000

    rooks move correctly

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,17 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let moves = [];
+        let row = board.findPiece(this).row;
+        let col = board.findPiece(this).col;
+        for(let i = 0; i < 8; i++) {
+            for(let j = 0; j < 8; j++) {
+                if((i === row || j === col) && !(i === row && j === col)) {
+                    moves.push(Square.at(i, j))
+                }
+            }
+        }
+
+        return moves;
     }
 }
