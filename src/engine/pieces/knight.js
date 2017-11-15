ab97fa369bf846fd20186ed5baea454e2823076e
Ben Ward <Ben.Ward@softwire.com>
2017-11-15 11:59:23 +0000

    Knights move

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -6,6 +6,9 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const current = board.findPiece(this);
+        return board.getSquares()
+            .filter(square => (Math.abs(square.row - current.row) === 2 && Math.abs(square.col - current.col) === 1)
+                           || (Math.abs(square.row - current.row) === 1 && Math.abs(square.col - current.col) === 2));
     }
 }
