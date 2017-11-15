cde8f66f0a82813a315934d8aa02481a13852eeb
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-11-15 11:14:23 +0000

    bishops move correctly

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,17 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let moves = [];
+        let row = board.findPiece(this).row;
+        let col = board.findPiece(this).col;
+        for(let i = 0; i < 8; i++) {
+            for(let j = 0; j < 8; j++) {
+                if((i + j === row + col || i - j === row - col) && !(i === row && j === col)) {
+                    moves.push(Square.at(i, j))
+                }
+            }
+        }
+
+        return moves;
     }
 }
