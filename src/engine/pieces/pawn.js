f749921cbe8ff890f8c0c953dae84a852477d58e
Ben Ward <Ben.Ward@softwire.com>
2017-11-15 10:42:15 +0000

    Made prawns move twice if they're fresh

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -5,15 +5,23 @@ import Square from '../square';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
+        this.hasMoved = false;
+    }
+
+    moveTo(board, newSquare) {
+      super.moveTo(board, newSquare);
+      this.hasMoved = true;
     }
 
     getAvailableMoves(board) {
-      let square  = board.findPiece(this);
-      if (this.player === Player.WHITE) {
-        return [new Square(square.row+1, square.col)]
-      }
-      else {
-        return [new Square(square.row-1, square.col)]
-      }
+        let square  = board.findPiece(this);
+        if (this.player === Player.WHITE) {
+            let moves = [Square.at(square.row+1, square.col)];
+            return moves.concat(this.hasMoved? [] : [Square.at(square.row+2, square.col)])
+        }
+        else {
+            let moves = [Square.at(square.row-1, square.col)];
+            return moves.concat(this.hasMoved? [] : [Square.at(square.row-2, square.col)])
+        }
     }
 }
